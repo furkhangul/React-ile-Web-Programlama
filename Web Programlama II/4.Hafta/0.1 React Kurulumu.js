c5e3e4b9deaf6b86ab659c18daa562606432d549
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-11 00:47:52 +0300

    1

---
+++ Web Programlama II/4.Hafta/0.1 React Kurulumu.js
@@ -0,0 +1,27 @@
+//React'a geçilmesi için belli indirmelerin yapılması gerekmetedir.
+
+//1-Node.js internette aratılarak indirilmesi ve kurulması gerekmektedir. "nodejs.gov"
+
+//2-Kurulum sırasında npm paket yöneticisinin indirildiğinden emin olunması gerekmektedir. 
+/* Olmadığı takdirde:
+ Arama Çubuğu -> "Gelişmiş Sistem Ayarları" -> Gelişmiş -> Ortam Değişkenleri -> Kullanıcı değişkenleri tablosundan -> Path içerisine gir ->
+ C:\Users\KullaniciAdi\AppData\Roaming\npm
+ adımları ile manuel olarak eklenmesi gerekmetedir.
+*/
+
+//3-Bu işlemlerin ardından "vite.dev" sitesine giriş yapılıp npm ile indirme bash kodunu kopyalıyoruz.
+
+//4- "npm create vite@latest" kodunu VS Code üzerinden açtığımız terminale girip. Projemizin bulunduğu dosya içerisinde çalıştırıyoruz.
+
+//5-Gelen koruma talebini "yes" ile geçiyoruz.
+
+//6-Proje adını  ve paket adını giriyoruz.
+
+//7-Sunulan Frameworkler arasından React'ı seçiyoruz
+
+//8-Varyant seçimlerinden yalnızca "JavaScript" olanı seçiyoruz.
+
+//9-Vite beta kullanım isteğini geçiyoruz. Ve indirme isteğini kabul ediyoruz.
+
+//Kurulum tamamlanır ve web adresimiz hazır hale gelir.
+
