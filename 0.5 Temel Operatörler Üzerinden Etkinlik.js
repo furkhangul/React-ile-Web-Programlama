a4354b11941cc84eefa670fb35b9507b721c2855
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-11 18:19:35 +0300

    5
    Added variables and prompt functionality for user input.

---
+++ 0.5 Temel Operatörler Üzerinden Etkinlik.js
@@ -0,0 +1,16 @@
+var egitmen = "Furkan Gül"
+console.log(egitmen)
+
+egitmen = egitmen + "bir eğitmendir."
+
+//Kullanıcıdan veri almak için prompot kullanılır.
+var egit = prompt("kullanicidan veri alma: ")
+
+//Bollean verileri için
+var dogurMu = true
+
+//Özel
+var sayi1 = prompt("Sayı 1: ");
+var sayi2 = prompt("Sayı 2: ");
+
+var toplam = sayi1 + sayi2
