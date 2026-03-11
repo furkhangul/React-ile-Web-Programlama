4739a82436d24d626924f48e90d56e92937eb2f7
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-11 18:34:29 +0300

    8

---
+++ 0.8 JS Fonksiyonların Kullanımı.js
@@ -0,0 +1,16 @@
+//Fonksiyonlar ile çalışmak:
+
+function SelamVer(){
+    console.log("Merhabalar !")
+}
+//Fonskiyon çağırılır.
+SelamVer()
+
+
+//Daha karmaşık:
+
+function Topla(a,b){
+    return a+b
+}
+var sonuc = Topla(3,5)
+alert("Sonuç:" + sonuc)
