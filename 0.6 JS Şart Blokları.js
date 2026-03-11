deda10bd685e2a208445ad332fb38c4467a99279
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-11 18:25:01 +0300

    6

---
+++ 0.6 JS Şart Blokları.js
@@ -0,0 +1,10 @@
+//Şart blokları: Basic herhangi bir yazılım dilindeki gibi if else üzerine kuruludur.
+var egitmen = "Furkan Gül"
+if (egitmen == "Furkan Gül"){
+    console.log("Doğru yoldasın !")
+    alert("Eğitmen: "+ egitmen)
+}
+else {
+    console.log("Yanlış yoldasın !")
+    alert("Tekrar seç !")
+}
