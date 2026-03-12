c09d077a05d1ce1eb332615e25207f4d9aa613ae
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-12 17:17:04 +0300

    17

---
+++ 1.7 JS GetElementByName Kullanımı.js
@@ -0,0 +1,6 @@
+//getElementsByName fonksiyonu ismine göre bulunmasını sağlar.
+
+//Daha önceden input değerimize name vermiştik. Değere ulaşmak için kullanılır genellikle.
+
+var veri = document.getElemenetsByName("user");
+alert(veri[0].value);
