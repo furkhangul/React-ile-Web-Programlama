30a2c814f5f04b0bc69ed09489aa3bd14241cc78
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-12 16:27:01 +0300

    14

---
+++ 1.4 JS DOM Manipülasyonu ile Veri Düzenleme.js
@@ -0,0 +1,18 @@
+//DOM Manipülasyonu :
+
+/*
+DOM mainpülasyonu, JS ile bir web sayfasının içeriğini, yapısını veya stillerini değiştirme işlemidir.
+Web sayfalarındaki HTML elemanları DOM adı verilen bir ağaç yapısı olarak temsil edilir ve JS  bu elemanlara erişip değiştirebilir.
+
+
+DOM Manipülasyonu ile :
+*HTML elemanlarını seçebiliriz.
+*İçeriği değiştirebiliriz.
+*Yeni bir eleman ekleyebiliriz.
+*Eleman silebiliriz.
+*CSS stilini değiştirebiliriz.
+*Kullanıcı etkileşiminie göre sayfayı güncelleyebiliriz.
+*/
+
+//Örnek DOM uygulamaları ile verileri değiştirme işlemi gerçeleştirdim:
+document.getElementById("bio").innerHTML = "Merhabalar benim adım Furkan Gül. 2004 yılında Siirt'te doğdum. Bandırma Onyedi Eylül Üniversitesinde Yazılım Mühendisliği öğrencisi olarak eğitim görmekteyim."
