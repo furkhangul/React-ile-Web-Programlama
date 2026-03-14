67e776328e50a9ee4dc494b1fd700238497a91f6
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-14 06:21:35 +0300

    22

---
+++ 2.2 JS Ecmascript Nedir.js
@@ -0,0 +1,11 @@
+//Ecmascript Nedir ?
+
+/*
+ECMAScript, web programlama dünyasında kullanılan bir standarttır.
+En basit şekilde söylemek gerekirse:
+-> JavaScript bir programlama dili iken
+-> ECMAScript JS'nin standart tanımıdır.
+Tarayıcılar ve yazılım dilleri bu buna Node.js de dahil,
+EcmaScript standartlarına göre hareket eder.
+
+*/
