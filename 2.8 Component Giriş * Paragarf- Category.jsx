a49b02a9036c7912ffd665bea675db0cf8e85bc0
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-16 08:10:24 +0300

    28

---
+++ 2.8 Component Giriş * Paragarf- Category.jsx
@@ -0,0 +1,15 @@
+import React , {Component} from "react"
+
+export default class Paragraf extends Component{
+    render(){
+        return(
+            <div>
+<p>
+Lorem ipsum dolor sit, amet consectetur adipisicing elit.<br />
+Aliquam quis aperiam blanditiis ipsum reiciendis debitis rerum.<br />
+Temporibus nemo at quo eos, corrupti error natus aperiam necessitatibus fugit earum qui? Magnam.
+</p>
+            </div>
+        )
+    }
+}
