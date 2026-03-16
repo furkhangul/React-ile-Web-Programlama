c660b752e7d3464289e3d787d4da132a359aacaf
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-16 08:10:58 +0300

    28

---
+++ 2.8 Component Giriş * main.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import {createRoot} from "react-dom/client"
+import App from "./components/App"
+
+var root = createRoot(document.getElementById("furkan"))
+root.render(<App/>)
