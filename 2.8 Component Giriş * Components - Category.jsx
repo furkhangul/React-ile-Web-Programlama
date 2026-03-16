a59f46ed4fed1460319ed053bb85f0078a4ea43f
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-16 08:09:38 +0300

    28

---
+++ 2.8 Component Giriş * Components - Category.jsx
@@ -0,0 +1,33 @@
+import React, {Component} from "react"
+
+export default class CategoryList extends Component{
+    render(){
+        return(
+            <>
+                <ul>
+                    <li>
+                        Matematik
+                    </li>
+                    <li>
+                        Türkçe
+                    </li>
+                    <li>
+                        İngilizce
+                    </li>
+                    <li>
+                        Fen Bilimleri
+                    </li>
+                    <li>
+                        Müzik
+                    </li>
+                    <li>
+                        Beden Eğitimi
+                    </li>
+                    <li>
+                        Sosyal Bilgiler    
+                    </li> 
+                </ul>
+            </>
+        )
+    }
+}
