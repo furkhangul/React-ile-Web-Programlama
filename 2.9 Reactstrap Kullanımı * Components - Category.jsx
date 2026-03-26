e91914b8e279cf5e735da9a90aa7715bd4424ba4
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-26 16:52:05 +0300

    29

---
+++ 2.9 Reactstrap Kullanımı * Components - Category.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from "react"
+
+export default class Category extends Component{
+    render(){
+        return(
+            <>
+                <h1 color="danger">Category List</h1>
+
+            </>
+        )
+        
+    }
+}
