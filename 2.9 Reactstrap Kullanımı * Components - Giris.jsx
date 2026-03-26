3981421ce98fbcb1ee9cda952ecae890f9f54f93
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-26 16:53:46 +0300

    29

---
+++ 2.9 Reactstrap Kullanımı * Components - Giris.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+
+export default class Giris extends Component{
+    render(){
+        return(
+            <>
+                <h1>Merhaba Furkan !</h1>
+                <p>Nasılsın ?</p>
+            </>
+        )
+    }
+}
