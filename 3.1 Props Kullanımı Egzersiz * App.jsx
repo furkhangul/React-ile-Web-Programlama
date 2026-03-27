3394cd8da2d1eb646578a7a83d3a01f63bfe53ff
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-27 13:49:35 +0300

    31

---
+++ 3.1 Props Kullanımı Egzersiz * App.jsx
@@ -0,0 +1,11 @@
+import UserInfo from "./Components/UserInfo"
+
+function App(){
+    return(
+        <div>
+            <UserInfo name="Furkan" surname="Gül" age={21} department="Software" salary={1535} email="gl56frkn@gmail.com" phone="+905418640498"/>
+            
+        </div>
+    )
+}
+export default App;
