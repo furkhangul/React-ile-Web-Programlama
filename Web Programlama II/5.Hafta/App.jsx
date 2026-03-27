d1b7c98840aa7c46206238c6a91f722b40c02cb4
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-27 13:14:15 +0300

    Create App.jsx

---
+++ Web Programlama II/5.Hafta/App.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import Info from "./Components/Info"
+
+function App(){
+    return(
+        <div>
+            <Info username="Furkan Gül" age={20} department="Software" salary={3475}/>
+        </div>
+    )
+}
+
+export default App
