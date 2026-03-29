097e1fb6510f3cfcba79740d1af25d910a2f53cc
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-03-29 15:46:05 +0300

    32

---
+++ 3.2 State Kullanımı * Components - UserInfo.jsx
@@ -0,0 +1,23 @@
+import {useState} from "react";
+function UserInfo(){
+
+     const state = {
+        Users: [
+            {UserId:1,UserName:"furkhangul",UserPass:"4686511f",UserEmail:"glfrkn@gmail.com"},
+            {UserId:2,UserName:"zodialks",UserPass:"dfjknsm",UserEmail:"dflgnkkn@gmail.com"},
+            {UserId:3,UserName:"kesdsua",UserPass:"gkjsl",UserEmail:"meauns@gmail.com"},
+
+        ]
+    }
+    return(
+        <>
+            {
+                state.Users.map(user =>
+                    <h1 key={user.UserId}>{user.UserName}<br/>{user.UserPass}<br/>{user.UserEmail}</h1>
+                )
+            }
+        </>
+    )
+}
+
+export default UserInfo
