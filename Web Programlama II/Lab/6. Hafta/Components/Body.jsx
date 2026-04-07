4e412852b7e89fcd9839d27fc462533da399dee7
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-04-07 14:15:34 +0300

    6

---
+++ Web Programlama II/Lab/6. Hafta/Components/Body.jsx
@@ -0,0 +1,14 @@
+function Body(){
+
+    return(
+        <>
+            <ul>
+                <li>Elma</li>   
+                <li>Armut</li> 
+                <li>Limon</li>
+             </ul>  
+        </>
+    )
+}
+
+export default Body
