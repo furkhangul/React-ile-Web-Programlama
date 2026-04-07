e0a74efa75ffc45243f5c707cc6ea940166e3964
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-04-07 14:15:47 +0300

    6

---
+++ Web Programlama II/Lab/6. Hafta/Components/Header.jsx
@@ -0,0 +1,28 @@
+
+
+function Header(){
+    return(
+        <>
+            <h1>Merhaba Dünya !</h1>
+            <ul>
+                <li>
+                    Ana Sayfa
+                </li>
+                <li>
+                    Ürünler
+                </li>
+                <li>
+                    Hakkımızda
+                </li>
+                <li>
+                    İletişim
+                </li>
+            </ul>
+            <br />
+            <hr />
+            <br />
+        </>
+    )
+}
+
+export default Header
