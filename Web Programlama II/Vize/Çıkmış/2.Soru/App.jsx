35a9e27a259aa026c51630dacecc16d51afd9414
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-04-13 00:04:18 +0300

    App

---
+++ Web Programlama II/Vize/Çıkmış/2.Soru/App.jsx
@@ -0,0 +1,12 @@
+import Urun from "./Components/Urun"
+
+function App(){
+    return(
+        <>
+            <Urun urunAdi="Dolma" urunFiyati={200}/>
+        </>
+    )
+}
+
+
+export default App;
