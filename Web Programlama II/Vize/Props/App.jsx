9bb56d67dd3ad0822e5006eb298298bc66a79a58
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-04-12 19:21:34 +0300

    App

--- Web Programlama II/Vize/Props/App.jsx
+++ Web Programlama II/Vize/Props/App.jsx
@@ -1,1 +1,13 @@
-inkşdf
+import Deneme from "./Components/Deneme"
+
+
+function App(){
+    return(
+        <>
+            <Deneme ad="Furkan" soyad="Gül" />
+        </>
+    )
+}
+
+
+export default App;
