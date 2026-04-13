16447098b516d590b50dbb8d2e429f8868629ed7
Furkan Gül <142796984+furkhangul@users.noreply.github.com>
2026-04-13 08:59:04 +0300

    3

---
+++ Web Programlama II/Vize/Çıkmış/3.soru.js
@@ -0,0 +1,10 @@
+/*
+Aşağıdaki senaryoya uygun olarak, JavaScript'te promise kullanarak bir fonksiyon yazınız:
+Bir restoranın online sipariş sistemi için bir fonksiyon yazmanız isteniyor. Bu fonksiyon, kullanıcıdan sipariş alacak ancak asenkron olarak hazırlanacak ve sonuç olarak siparişin hazırlanıp hazır olup olmadığını bildirecek. Fonksiyonun adı "siparisHazirla" olacak ve aşağıdaki özellikleri sağlamalıdır:
+a. "siparis" adında bir nesne: Bu nesne, kullanıcının verdiği siparişi temsil edecek "urunAdi" ve "adet" özelliklerini içerecek.
+b. Verilen sipariş nesnesini alarak, belirli bir süre sonra siparişin hazır olup olmadığını kontrol edecek bir promise döndürmelidir.
+c. Eğer sipariş hazır ise (başarılı), "Siparişiniz hazır!" mesajını döndürmelidir.
+d. Eğer sipariş henüz hazır değilse (başarısız), "Siparişiniz hazırlanıyor, lütfen bekleyin." mesajını döndürmelidir.
+
+Not: Siparişin hazırlanması için belirlenen süreyi ve hazırlık durumunu belirlemek için rastgele değerler kullanabilirsiniz. Gerekli javascript kodlarını yazınız.
+*/
